41e43b7f656e4f8229b2741b18a8b365d5164ae6
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-09-01 17:01:30 -0300

    bugfix: blank page on index.html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
-  const win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile("./build/index.html");
-  //win.loadURL("http://localhost:3000");
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    transparent: true,
+    webPreferences: { nodeIntegration: false, contextIsolation: true },
+  });
+  win.loadFile("build/index.html");
+  //win.loadURL(`"http://localhost:3000"`);
 }
 
 app.on("ready", createWindow);
